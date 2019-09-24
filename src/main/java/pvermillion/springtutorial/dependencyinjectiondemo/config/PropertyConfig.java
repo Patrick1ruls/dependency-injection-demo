4ce60a61277fa36e5a54ee0fbe916f3950320380
Patrick1ruls <patrickvermillion555@gmail.com>
2019-09-24 15:24:56 -0500

    Condensed all property files into the spring application.property file

--- src/main/java/pvermillion/springtutorial/dependencyinjectiondemo/config/PropertyConfig.java
+++ src/main/java/pvermillion/springtutorial/dependencyinjectiondemo/config/PropertyConfig.java
@@ -1,26 +1,13 @@
 package pvermillion.springtutorial.dependencyinjectiondemo.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 import pvermillion.springtutorial.dependencyinjectiondemo.examplebeans.FakeDataSource;
 import pvermillion.springtutorial.dependencyinjectiondemo.examplebeans.FakeJmsBroker;
 
 @Configuration
-// @PropertySource({"classpath:datasource.properties", "classpath:jms.properties"}) // Looking in resource folder for this file
-@PropertySources({ // Another way to show multiple property files
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-    @Autowired
-    Environment env;
 
     @Value("${pv.username}")
     String user;
@@ -43,7 +30,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(env.getProperty("USERNAME")); // YOOO I CAN SET UP ENVIRONMENT VARIABLES LIKE THIS???!!!!
+        fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
@@ -58,9 +45,4 @@ public class PropertyConfig {
         return jmsBroker;
     }
 
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
-    }
 }
